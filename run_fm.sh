2dacda76dac30fe710cfaebc896839644fc7bef8
李明星 <limingxing03@meituan.com>
2018-07-22 17:32:04 +0800

    add

---
+++ run_fm.sh
@@ -0,0 +1,2 @@
+
+python ./python/fm.py ./data/train.fm.txt ./data/test.fm.txt ./data/featindex.txt ./output/result_file.txt ./output/model_file.txt ./output/train.log.txt ./output/test.log.txt ./output/train.dl.txt ./output/test.dl.txt
